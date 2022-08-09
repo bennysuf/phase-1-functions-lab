9112259557706f46cab331e1ebaea09c57d71a1d
Benny Sufrin <bennysuf@gmail.com>
2022-08-09 16:49:21 -0400

    commit tree 600dc49bcc7a614ce9b552db0542fc652412d837

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(num) {
+ return num >= 42 ? num - 42: 42 - num  
+};
+
+function distanceFromHqInFeet(num) {
+ return distanceFromHqInBlocks(num) * 264
+};
+
+function distanceTravelledInFeet(start, destination) {
+    return start > destination ? (start - destination) * 264: (destination - start) * 264
+};
+
+function calculatesFarePrice(start, destination) {
+    const dis = distanceTravelledInFeet(start, destination);
+    if (dis <= 400) {
+        return 0
+    } else if (dis > 400 && dis < 2000) {
+        return ((dis) - 400) * 0.02
+    } else if (dis > 2000 && dis < 2500) {
+        return 25
+    } else {
+       return 'cannot travel that far'
+    };
+};
